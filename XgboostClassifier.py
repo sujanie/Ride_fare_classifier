8f40ba0c38c86b82e00b3870803511504aa425e2
sujanie <36542245+sujanie@users.noreply.github.com>
2020-06-19 23:39:32 +0530

    Update XgboostClassifier.py

--- XgboostClassifier.py
+++ XgboostClassifier.py
@@ -73,45 +73,36 @@ def cleanData(data, dropcols):
     data['period_of_day']= data['pickup_hour'].map(map_hours)
     data['drop_day_of_week'] = data['drop_day_of_week'].apply(lambda x: day_dict[x])
     data['manhattan_dist'] = manhattan_distance(data['pick_lat'].values,data['pick_lon'].values,data['drop_lat'].values,data['drop_lon'].values)
-    #data['center_latitude'] = (data['pick_lat'].values + data['drop_lat'].values) / 2
-    #data['center_longitude'] = (data['pick_lon'].values + data['drop_lon'].values) / 2
     data['total_time']=(data['drop_time']-data['pickup_time']).astype('timedelta64[s]')
-    data['duration'] = data['duration'].fillna(value = data['total_time'])
-    data['additional_fare']=data['additional_fare'].fillna(value=round(data['additional_fare'].mode()[0]))
+    
     data['bearing_dist'] = bearing_array(data['pick_lat'].values, data['pick_lon'].values,
                                                 data['drop_lat'].values, data['drop_lon'].values)
     data['haversine_dist'] = haversine_array(data['pick_lat'].values, data['pick_lon'].values,
                                         data['drop_lat'].values, data['drop_lon'].values)
 
-    #data['add_fare']=data['meter_waiting_till_pickup'/data['additional_fare']
-    #data['diff']=np.abs(data['total_time']-data['duration'])]
-
-
-    imp = IterativeImputer(RandomForestRegressor(n_estimators=5), max_iter=5, random_state=1)
     to_train = ['meter_waiting', 'meter_waiting_fare', 'meter_waiting_till_pickup','fare']
     # perform filling
     for k in to_train:
         mode_df = round(data[k].mode()[0])
         mode_df = round(data[k].mean())
         data[k]=data[k].fillna(mode_df)
-    #data[to_train] = pd.DataFrame(imp.fit_transform(data[to_train]), columns=to_train)
-
+    data['duration'] = data['duration'].fillna(value = data['total_time'])
+    data['additional_fare']=data['additional_fare'].fillna(value=round(data['additional_fare'].mode()[0]))
     data['manhattan_dist'] =data['manhattan_dist'].mask(data['manhattan_dist']==0).fillna(1)
     data['duration'] = data['duration'].mask(data['duration']==0).fillna(1)
+    
     data['effective_fare'] = data['fare'] -  data['meter_waiting_fare']-data['additional_fare']
     data['cost_per_mile']=data['effective_fare']/data['manhattan_dist']
     data['cost_per_duration']=data['effective_fare']/data['duration']
-    #data['manhattan_dist'] = np.log1p(data['manhattan_dist'])
-    #data['meter_waiting_fare'] = np.log1p(data['meter_waiting_fare'])
+ 
     data.drop(dropcols, axis=1, inplace=True)
-    #plt.show()
+    
     data=poly_transform(data)
     return data
 
 def poly_transform(data):
     poly = PolynomialFeatures(interaction_only=True)
     sc = RobustScaler()
-    to_check1=['duration','manhattan_dist','fare']
     to_check=['meter_waiting_till_pickup','additional_fare','period_of_day']
     to_crosscheck = ['meter_waiting', 'meter_waiting_fare']
     feats=['additional_fare','meter_waiting_fare','meter_waiting','meter_waiting_till_pickup','cost_per_duration','cost_per_mile']
@@ -121,9 +112,6 @@ def poly_transform(data):
     crossed_feats1 = poly.fit_transform(data[to_check].values)
     crossed_feats1 = pd.DataFrame(crossed_feats1)
     data = pd.concat([data, crossed_feats1], axis=1)
-    #crossed_feats2 = poly.fit_transform(data[to_check1].values)
-    #crossed_feats2 = pd.DataFrame(crossed_feats2)
-    #data = pd.concat([data, crossed_feats2], axis=1)
     sc_data = sc.fit_transform(data[feats])
     sc_data=pd.DataFrame(sc_data)
     data = pd.concat([data, sc_data], axis=1)
@@ -165,7 +153,7 @@ def test(classifier):
     fields = ["tripid", "prediction"]
     obj = csv.DictWriter(csvfile, fieldnames=fields)
     obj.writeheader()
-    # print(x['tripid'][0],x_test[0],y_test[0],len(y_test),len(x))
+   
     for i in range(len(x)):
         rowrecord = {}
         rowrecord["tripid"] = x['tripid'][i]
@@ -180,20 +168,15 @@ y = dataset['label']
 X = cleanData(dataset, ['pickup_time', 'drop_time', 'label', 'pickup_year', 'drop_year', 'pickup_month', 'drop_month','effective_fare'])
 X = X.iloc[:, 1:30].values
 
-
-#imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-#imputer = imputer.fit(X[:, 0:20])
-#X[:, 0:20] = imputer.transform(X[:, 0:20])
 # fit model no training data
 ftwo_scorer = make_scorer(fbeta_score, average='macro',beta=1)
-gnb =XGBClassifier(learning_rate =0.15, n_estimators=300, max_depth=9,
+xgb =XGBClassifier(learning_rate =0.15, n_estimators=300, max_depth=9,
  min_child_weight=1, gamma=0, subsample=0.9, colsample_bytree=0.75,
 objective='binary:logistic', nthread=4,  seed=27,reg_alpha= 1,
 eval_metric=["auc","error","logloss"],max_delta_step=0,tree_method='approx',reg_lambda=1.0,sketch_eps= 0.001,validate_parameters=True)
-#subsample=0.85, colsample_bytree=0.75 rate=0.3 depyh 9
-#gnb=BaggingClassifier(n_estimators=150,random_state=27)
+
 skf = StratifiedKFold(n_splits=10,random_state=1020, shuffle=True)
-#skf = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
 scores=[]
 mscores=[]
 for train_index, test_index in skf.split(X, y):
@@ -203,10 +186,10 @@ for train_index, test_index in skf.split(X, y):
     X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    gnb.fit(X_train, y_train,early_stopping_rounds=100, eval_set=[(X_test, y_test)],verbose=True)
-    #gnb.fit(X_train, y_train)
-    f1 = fbeta_score(y_train, gnb.predict(X_train) , average='macro', beta=1)
-    y_pred2 = gnb.predict(X_test)
+    xgb.fit(X_train, y_train,early_stopping_rounds=100, eval_set=[(X_test, y_test)],verbose=True)
+   
+    f1 = fbeta_score(y_train, xgb.predict(X_train) , average='macro', beta=1)
+    y_pred2 = xgb.predict(X_test)
 
     f2=fbeta_score(y_test, y_pred2, average='macro', beta=1)
     print("Performance sur le train : ", f1)
@@ -214,8 +197,7 @@ for train_index, test_index in skf.split(X, y):
     cm1 = confusion_matrix(y_test, y_pred2)
 
     print("confusion matrix/n")
-    # probs=gnb.predict_proba(X_test)
-    # probs = probs[:, 1]
+  
     print(cm1)
 
     print("/n classification report /n")
@@ -230,4 +212,4 @@ for train_index, test_index in skf.split(X, y):
 print(np.mean(scores))
 
 print(np.mean(mscores))
-test(gnb)
+test(xgb)
